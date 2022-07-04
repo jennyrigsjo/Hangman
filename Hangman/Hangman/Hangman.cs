09d14c49b485ac3442120e92af89005d8755b043
jennyrigsjo <jenny.rigsjo@live.com>
2022-07-04 10:28:16 +0200

    Removed unnecessary usings

--- Hangman/Hangman/Hangman.cs
+++ Hangman/Hangman/Hangman.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Runtime.CompilerServices;
-[assembly:InternalsVisibleTo("HangmanTests")]
+﻿using System.Text;
 
 namespace HangmanGame
 {
